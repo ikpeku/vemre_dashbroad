f75dadffd95f586cfbbee96c58fbbda5acbfc7d0
ikpeku <ikpedaniel07@gmail.com>
2025-04-10 19:23:17 +0100

    add favico icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,18 @@ export const metadata: Metadata = {
   title: 'Vemre',
   description: 'Vemre admin dashbroad',
   generator: 'vemre',
+  
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      {url:"/android-chrome-192x192.png", sizes:"192x192", type:"image/png"},
+      {url:"/android-chrome-512x512.png", sizes:"512x512", type:"image/png", }
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png',},
+    ],
+  },
+  
 }
 
 export default function RootLayout({
